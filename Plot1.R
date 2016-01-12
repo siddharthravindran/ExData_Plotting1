5307b3514d3f90fb9cced2117ac51925a8c3b1c2
siddharthravindran <siddharthravindran824@gmail.com>
2016-01-11 19:36:30 -0800

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,3 @@
+> png("plot1.png", width = 480, height = 480)
+> hist(electricpowernew$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col = "Red")
+> dev.off()
